5e391c8e182d162edffd48c7857dabc143d51ef2
timislearning <tim.mitzen@gmail.com>
2019-11-15 11:22:31 -0800

    starting the farmerdashboard form

--- farm-fresh-product-app/src/components/Dashboard/FarmerDashboard.js
+++ farm-fresh-product-app/src/components/Dashboard/FarmerDashboard.js
@@ -7,9 +7,27 @@ const FarmerDashboard = () => {
       <h2>Farmer's Dashboard</h2>
       <h3>Produce</h3>
       <Form>
-         
+         <label htmlFor="produce">Produce</label>
+         <Field className='produce' type='text' name='produce' placeholder='Produce Name'/>
+         <label htmlFor="qty">Quantity</label>
+         <Field className='qty' type='number' name='quantity' placeholder='Quantity'/>
+         <label htmlFor="org">Organic</label>
+         <Field className='organic' type='checkbox' name='organic' />
+         <label htmlFor="nongmo">Non Gmo</label>
+         <Field className='nonGmo' type='checkbox' name='nonGmo'/>
       </Form>
-      
-      </div>
+   </div>
    );
-}
+},
+
+const FormikFarmDashboard = withFormik({
+   mapPropsToValues(values){
+     return{ 
+        produce: values.produce || '',
+        quantity: values.quantity || '',
+        organic: values.organic || '',
+
+   }
+
+   }
+})
