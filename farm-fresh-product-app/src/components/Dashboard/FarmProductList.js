e2265782e37478d890a3ce85576219650634c1e2
timislearning <tim.mitzen@gmail.com>
2019-11-15 09:32:16 -0800

    Setup components and sass for farmerdashboard and productlist

---
+++ farm-fresh-product-app/src/components/Dashboard/FarmProductList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FarmerProductList = () =>{
+   return(
+      <div>
+         <h2>Farm Project List</h2>
+      </div>
+   )
+}
+export default FarmerProductList;
