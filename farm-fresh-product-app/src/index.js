eb3ee34275838e23140269e12fc6f0d33550af86
vy3191 <venkyafhs@gmail.com>
2019-11-16 10:52:50 -0500

    Created global store using Redux - wiredup with farmer register

--- farm-fresh-product-app/src/index.js
+++ farm-fresh-product-app/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { reducer as farmerRegister} from '../src/reducers/farmerRegister';
 import './index.scss';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import App from './App';
 
+const reducer = combineReducers({farmRegister:farmerRegister});
+const middleware = applyMiddleware(thunk,logger);
+const store = createStore(reducer,middleware);
 
 
 ReactDOM.render(
 <Router>
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>  
 </Router>, document.getElementById('root'));
 
