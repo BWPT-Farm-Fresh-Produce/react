41d454141b26a26c770a89c4f6121e070be643eb
timislearning <tim.mitzen@gmail.com>
2019-11-17 16:46:18 -0800

    Some Changes

--- farm-fresh-product-app/src/components/Dashboard/FarmerDashboard.js
+++ farm-fresh-product-app/src/components/Dashboard/FarmerDashboard.js
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import Background from '../../assets/images/background.jpg'
+
 import axios from 'axios';
 import '../Dashboard/farmerdashboard.scss'
 const FarmerDashboard = () => {
    const [farmerProduce, setFarmerProduce] = useState([]);
    useEffect(()=>{
       axios
-      .get('https://cors-anywhere.herokuapp.com/https://swapi.co/api/people')
+      .get('https://farm-fresh-bw.herokuapp.com/api/farmers/produce/categories')
       .then(response=>{
          console.log(response)
          setFarmerProduce(response.data.results)
@@ -16,11 +16,11 @@ const FarmerDashboard = () => {
       })
    },[]
    )
+
    return(
       
       <div className='dashboard'>
-      
-      <button className='logoff-btn'>Log Off</button>
+      <button className='logoff-btn'>Log Out</button>
       <button className='edit'>Edit</button>
       <button className='delete'>Delete</button>
       <button className='add'>Add</button>
