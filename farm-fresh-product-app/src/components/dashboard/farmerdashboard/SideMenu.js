81567a9b6619af6b6c6b6050726270a163108c35
timislearning <tim.mitzen@gmail.com>
2019-11-19 18:23:40 -0800

    Added Title for menu

--- farm-fresh-product-app/src/components/dashboard/farmerdashboard/SideMenu.js
+++ farm-fresh-product-app/src/components/dashboard/farmerdashboard/SideMenu.js
@@ -5,7 +5,7 @@ const Sidebar =()=>{
 
    return(
    <div className="vertical-menu">
-   <button className='nonactive'>Manage Produce</button>
+   <button className='non-active'>Manage Produce</button>
   <button className="active">Add Farm Item</button>
   <button className='active'>Edit Farm Item</button>
   <button className='active'>Delete Farm Item</button>
