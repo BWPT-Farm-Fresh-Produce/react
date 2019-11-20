7d514c29ceb498d11b08452e304cd54cdf0e30e9
timislearning <tim.mitzen@gmail.com>
2019-11-19 17:23:47 -0800

    SideMenu'

--- farm-fresh-product-app/src/components/dashboard/FarmerDashboard.js
+++ farm-fresh-product-app/src/components/dashboard/FarmerDashboard.js
@@ -3,12 +3,13 @@ import {NavLink} from 'react-router-dom'
 import axios from 'axios';
 import Header from '../header/Header';
 import '../dashboard/farmerdashboard.scss';
+import SideMenu from '../dashboard/SideMenu'
 
 const FarmerDashboard = () => {
    const [farmerProduce, setFarmerProduce] = useState([]);
    useEffect(()=>{
       axios
-      .get('https://farm-fresh-bw.herokuapp.com/api/farmers/produce/categories', {
+      .get('https://cors-anywhere.herokuapp.com/https://swapi.co/api/people', {
          headers: {
             authorization: localStorage.getItem('token')
          }
@@ -27,10 +28,11 @@ const FarmerDashboard = () => {
       
       <div className='dashboard'>
        <Header />
+
       <h1 className='farmdTitle'>Farmer's Dashboard</h1>
       <h3>Produce</h3>
       <div className='farmerproduce'>
-      
+      <SideMenu />
       {farmerProduce.map((farmer,index)=>(
          <div key={index} className='farmerProducts'>
          <p className='productcontent'>{farmer.name}</p>
---
+++ farm-fresh-product-app/src/components/dashboard/SideMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../dashboard/farmerdashboard.scss';
+
+const Sidebar =()=>{
+
+   return(
+   <div className="vertical-menu">
+  <button className="active">Add Farm Item</button>
+  <button className='active'>Edit Farm Item</button>
+  <button className='active'>Delete Farm Item</button>
+   </div>
+   );
+
+}
+
+export default Sidebar;
