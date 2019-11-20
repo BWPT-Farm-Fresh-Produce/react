414463c0f0906315f46b708f48a7d05ae5b888da
vy3191 <venkyafhs@gmail.com>
2019-11-20 08:19:11 -0500

    Add farm component wired-up with app.js

--- farm-fresh-product-app/src/components/dashboard/farmerdashboard/SideMenu.js
+++ farm-fresh-product-app/src/components/dashboard/farmerdashboard/SideMenu.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './farmerdashboard.scss';
 
 const Sidebar =()=>{
 
    return(
    <div className="vertical-menu">
-  <button className="active">Add Farm Item</button>
+   <Link to="/farmer-dashboard-addfarm">
+      <button className="active">Add Farm Item</button>
+  </Link>
   <button className='active'>Edit Farm Item</button>
   <button className='active'>Delete Farm Item</button>
    </div>
